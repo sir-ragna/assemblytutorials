f35c32790810f21fc2709c0b68e597288bf16afc
Robbe Van der Gucht <sir.ragna@gmail.com>
2022-06-06 18:28:53 +0200

    x64 update for lesson 2

--- code/lesson2/helloworld.asm
+++ code/lesson2/helloworld.asm
@@ -1,22 +1,22 @@
 ; Hello World Program - asmtutor.com
-; Compile with: nasm -f elf helloworld.asm
-; Link with (64 bit systems require elf_i386 option): ld -m elf_i386 helloworld.o -o helloworld
+; Compile with: nasm -f elf64 helloworld.asm
+; Link with: ld -m elf_x86_64 helloworld.o -o helloworld
 ; Run with: ./helloworld
 
 SECTION .data
-msg     db      'Hello World!', 0Ah
+msg     db      'Hello World!', 0Ah ; assign msg variable with your message string
 
 SECTION .text
 global  _start
 
 _start: 
 
-    mov     edx, 13
-    mov     ecx, msg
-    mov     ebx, 1
-    mov     eax, 4
-    int     80h
+    mov     rax, 1   ; invoke SYS_WRITE (kernel opcode 1)
+    mov     rdi, 1   ; file descriptor to write to. STDOUT in this case
+    mov     rsi, msg ; move the memory address of our message string into ecx
+    mov     rdx, 13  ; number of bytes to write - one for each letter plus 0Ah (line feed character)
+    syscall
 
-    mov     ebx, 0      ; return 0 status on exit - 'No Errors'
-    mov     eax, 1      ; invoke SYS_EXIT (kernel opcode 1)
-    int     80h
+    mov     rax, 60  ; invoke SYS_EXIT
+    mov     rdi, 0   ; exit with exit code 0
+    syscall
