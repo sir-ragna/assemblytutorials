db08ef13496301c0359bb4266e3e6345ef7cf118
Robbe Van der Gucht <sir.ragna@gmail.com>
2022-06-07 23:32:46 +0200

    add lesson 6

--- code/lesson6/helloworld-inc.asm
+++ code/lesson6/helloworld-inc.asm
@@ -1,23 +1,22 @@
-; Hello World Program (NULL terminating bytes)
-; Compile with: nasm -f elf helloworld-inc.asm
-; Link with (64 bit systems require elf_i386 option): ld -m elf_i386 helloworld-inc.o -o helloworld-inc
+; Hello World Program (External file include)
+; Compile with: nasm -f elf64 helloworld-inc.asm
+; Link with: ld -m elf_x86_64 helloworld-inc.o -o helloworld-inc
 ; Run with: ./helloworld-inc
 
-%include        'functions.asm'
+%include        'functions.asm'                             ; include our external file
 
 SECTION .data
-msg1    db      'Hello, brave new world!', 0Ah, 0h          ; NOTE the null terminating byte
-msg2    db      'This is how we recycle in NASM.', 0Ah, 0h  ; NOTE the null terminating byte
+msg1    db      'Hello, brave new world!', 0Ah, 0           ; our first message string
+msg2    db      'This is how we recycle in NASM.', 0Ah, 0   ; our second message string
 
 SECTION .text
 global  _start
     
 _start: 
-
-    mov     eax, msg1        
-    call    sprint          
-
-    mov     eax, msg2
-    call    sprint
-
-    call    quit
+    mov     rdi, msg1  ; move the address of our first message string into rdi
+    call    sprint     ; call our string printing function
+    
+    mov     rdi, msg2  ; move the address of our second message string into rdi
+    call    sprint     ; call our string printing function
+    
+    call    quit       ; call our quit function
